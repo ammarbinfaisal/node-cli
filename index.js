df676bd0c801363b9e979a38a10b89e512d3e1e6
ammar <ammarbinfaisal@bitbucket.org>
2018-04-15 13:05:10 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const yargs = require('yargs');
+const notes = require('./notes.js');
+const fs = require('fs');
+const echo = console.log;
+
+const command = yargs.argv._[0];
+const title = yargs.argv.title || yargs.argv._[1];
+const body = yargs.argv.body;
+
+
+switch (command) {
+     case 'add':
+          echo('adding the note...');
+          if (body === undefined) {
+               throw new Error('Illegal command!');
+          }
+          notes.add({
+               title,
+               body
+          });
+          break;
+     case 'read':
+          echo('reading the note...');
+          notes.read(title);
+          break;
+     case 'list':
+          echo('listing all notes...');
+          notes.list();
+          break;
+     case 'remove':
+          echo('removing the note...');
+          notes.remove(title);
+          break;
+     default:
+          echo('wrong command')
+          break;
+}
---
+++ notes.js
@@ -0,0 +1,59 @@
+let notes = [];
+const _ = require('lodash');
+const fs = require('fs');
+
+const readNotes = () => {
+     try {
+          notes = JSON.parse(fs.readFileSync('./notes.json', {
+               encoding: 'utf-8'
+          }));
+     } catch (error) {
+          return;
+     }
+     notes = JSON.parse(fs.readFileSync('./notes.json', {
+          encoding: 'utf-8'
+     }));
+}
+
+const writeNotes = () => {
+     fs.writeFileSync('./notes.json', JSON.stringify(notes), {
+          encoding: 'utf-8'
+     });
+}
+
+module.exports = {
+     add(note) {
+          readNotes();
+          if (_.find(notes, { title: note.title })) {
+               //throw new Error => yeh thooda darauna lagta hai/I mean that error thrown in the terminal scares me
+               console.log('ERROR: A note wih same title already exists');
+               return;
+          }
+          notes.push(note);
+          writeNotes();
+     },
+     read(note) {
+          readNotes();
+          let requiredNote = _.find(notes, {
+               title: note
+          });
+          console.log(`Title: "${requiredNote.title}"\nBody: "${requiredNote.body}"`);
+     },
+     list() {
+          readNotes();
+          notes.forEach((note, index) => {
+               console.log(`NOTE ${index + 1}\n`);
+               console.log(`Title: "${note.title}"\nBody: ${note.body}\n\n`)
+          });
+     },
+     remove(note) {
+          readNotes();
+          if (_.find(notes, { title : note })) {   
+               notes.splice(_.findIndex(notes, { title: note }), 1);
+               console.log(notes)
+          } else {
+               console.log('ERROR!: no such note exists');
+          }
+          writeNotes();
+     }
+}
